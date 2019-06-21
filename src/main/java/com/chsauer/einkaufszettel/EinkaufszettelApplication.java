aa463f2d930fcdf375ca29844cdf5567556675f2
ChristianSauer <christian.sauer@email.de>
2019-06-21 21:58:50 +0200

    initial

---
+++ src/main/java/com/chsauer/einkaufszettel/EinkaufszettelApplication.java
@@ -0,0 +1,13 @@
+package com.chsauer.einkaufszettel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufszettelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EinkaufszettelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chsauer/einkaufszettel/EinkaufszettelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chsauer.einkaufszettel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EinkaufszettelApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
